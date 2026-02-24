3db645e8d742246f4a677a5d56bc16a946ad6e49
J0hnV33 <jvk3@student.le.ac.uk>
2026-02-24 10:47:52 +0000

    Update align_loop.sh

--- scripts/align_loop.sh
+++ scripts/align_loop.sh
@@ -16,7 +16,7 @@ INDEX="/home/j/jvk3/camp_alice/ref/hg19_index"
 OUT_DIR="/scratch/alice/j/jvk3/camp_alice/aligned"
 mkdir -p $OUT_DIR
 
-# This loop looks at your list and only processes SRRs that have FASTQ files ready
+# Loop looks at your list and only processes SRRs that have FASTQ files ready
 sed -n '1,734p' ~/camp_alice/meta/srr_list.txt | while read SRR; do
     # Only run if the FASTQ exists AND the sorted BAM doesn't exist yet
     if [ -f "${FASTQ_DIR}/${SRR}_1.fastq" ] && [ ! -f "${OUT_DIR}/${SRR}_sorted.bam" ]; then
@@ -25,7 +25,7 @@ sed -n '1,734p' ~/camp_alice/meta/srr_list.txt | while read SRR; do
         # Align
         bowtie2 -p 8 -q -x $INDEX -1 ${FASTQ_DIR}/${SRR}_1.fastq -2 ${FASTQ_DIR}/${SRR}_2.fastq -S ${OUT_DIR}/${SRR}.sam
         
-        # Convert, Sort, and Clean
+        # convert, sort, and clean
         samtools view -bS ${OUT_DIR}/${SRR}.sam > ${OUT_DIR}/${SRR}.bam
         samtools sort ${OUT_DIR}/${SRR}.bam -o ${OUT_DIR}/${SRR}_sorted.bam
         rm ${OUT_DIR}/${SRR}.sam ${OUT_DIR}/${SRR}.bam
