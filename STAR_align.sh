fd627ac7ce9359987dbeafc7762580fe741851b8
Asadgattab12 <aakg1@student.le.ac.uk>
2026-04-27 00:42:29 +0100

    Set paths for genome and fastq directories
    Updated paths for genome, fastq, and output directories.

--- STAR_align.sh
+++ STAR_align.sh
@@ -1,3 +1,6 @@
+#STAR ALIGNMENT code
+# last update 26/04/2026
+
 #!/bin/bash
 #SBATCH --job-name=STAR_alignment
 #SBATCH --nodes=1
@@ -15,9 +18,11 @@ module load star/2.7.11a-5kstrrw
 module load samtools/1.17-wenuvv5
 
 # paths/use own paths
-#GENOME_DIR="/scratch/alice/a/aakg1/genome_files/star_index" #insert own pathways here
-#FASTQ_DIR="/scratch/alice/a/aakg1/SRA_project/fastq"
-#OUT_DIR="/scratch/alice/a/aakg1/SRA_project/alignment_results"
+GENOME_DIR="/scratch/alice/a/aakg1/genome_files/star_index" 
+FASTQ_DIR="/scratch/alice/a/aakg1/SRA_project/fastq"
+OUT_DIR="/scratch/alice/a/aakg1/SRA_project/alignment_results"
+
+#generate output directory
 
 mkdir -p $OUT_DIR
 
@@ -36,7 +41,7 @@ SAMPLE=$(basename $R1 _1.fastq.gz)
          --outSAMtype BAM SortedByCoordinate \ #setting the parameters
          --quantMode GeneCounts \
          --outSAMunmapped Within \
-         --limitBAMsortRAM 30000000000 #indicates how much memory allowed to use in RAM
+         --limitBAMsortRAM 30000000000 #indicates how much memory allowed to use in RAM(bytes)
 
  # 5. Index the BAM
     samtools index ${OUT_DIR}/${SAMPLE}_Aligned.sortedByCoord.out.bam
